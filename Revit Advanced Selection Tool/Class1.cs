50b7155d904fba0824d0223cbb2a03e705f560f0
r.zakrevskiy <r.zakrevskiy@W398>
2025-12-04 14:45:02 +0300

    Добавьте файлы проекта.

---
+++ Revit Advanced Selection Tool/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit_Advanced_Selection_Tool
+{
+    public class Class1
+    {
+    }
+}
